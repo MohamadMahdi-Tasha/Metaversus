f348095a0ee12e3349f798af1096f4d38e558c13
Mahdi Tasha <imwhdiiii@gmail.com>
2023-06-18 13:17:24 +0330

    made changes in rendering of page

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+// Codes By Mahdi Tasha
+// Creating and exporting functional component of whole app, as default
+export default function App() {
+    // Returning JSX
+    return (
+        <h1>Hello From App Component</h1>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
+// Codes By Mahdi Tasha
+// Importing Part
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import App from "./app.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <h1>Hello</h1>
-  </React.StrictMode>,
-)
+// Variables
+const rootElement = document.getElementById('root') as HTMLElement;
+const reactRootElement = ReactDOM.createRoot(rootElement);
+
+// Wrapping whole app inside Strict mode of React and rendering them in root element
+reactRootElement.render(<React.StrictMode><App /></React.StrictMode>)
