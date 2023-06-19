9cce83cdc668d449d2e9bc51979898ab5e6b2298
Mahdi Tasha <imwhdiiii@gmail.com>
2023-06-19 15:47:12 +0330

    fixed som elil bugs

--- src/components/fifthSectionComponent.tsx
+++ src/components/fifthSectionComponent.tsx
@@ -12,8 +12,8 @@ export default function FifthSectionComponent():JSX.Element {
     return (
         <SectionComponent className={'lg:mt-[200px] mt-[100px] flex lg:flex-row flex-col items-center'}>
             <div className={'lg:w-[50%] w-full'}>
-                <PreTitlesComponent centred>| Whats New?</PreTitlesComponent>
-                <TitlesComponent centred>What's new about Metaversus?</TitlesComponent>
+                <PreTitlesComponent>| Whats New?</PreTitlesComponent>
+                <TitlesComponent>What's new about Metaversus?</TitlesComponent>
                 <div className={'flex items-start lg:flex-row flex-col gap-[46px]'}>
                     <div>
                         <div className={'bg-[rgba(256,256,256,.08)] w-[70px] h-[70px] text-white mb-[24px] rounded-[24px] flex items-center justify-center'}><IconComponent type={'image'}/></div>
--- src/components/sixthSectionComponent.tsx
+++ src/components/sixthSectionComponent.tsx
@@ -10,8 +10,8 @@ export default function SixthSectionComponent():JSX.Element {
     // Returning JSX
     return(
         <SectionComponent className={'lg:mt-[200px] mt-[100px]'}>
-            <PreTitlesComponent>| People on the World</PreTitlesComponent>
-            <TitlesComponent>Track friends around you and invite them to play together in the same world</TitlesComponent>
+            <PreTitlesComponent centred>| People on the World</PreTitlesComponent>
+            <TitlesComponent centred>Track friends around you and invite them to play together in the same world</TitlesComponent>
             <img src={BottomImage} alt="ilustration of People on the World"/>
         </SectionComponent>
     );
